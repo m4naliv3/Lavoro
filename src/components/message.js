75abcbe5ac5aeb935e1f731da9f47413ed1d6c4b
Amanda Little <amanda.little@scorpion.co>
2019-10-17 22:18:17 -0400

    WIP on messages

--- src/components/message.js
+++ src/components/message.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 
 export default class MessageItem extends Component {
+  renderClassName(message) {
+    if(message.direction) {
+      return "message-item outgoing"
+    }
+    else return "message-item incoming"
+  }
+
   render(){
     var message = this.props.Message;
     if(!message) return null;
     return (
-      <div className="message-item">
-        <span>{message.MessageText}</span>
-        <br/>
-        <span>{message.Author}</span>
-        <span>{message.SentDate}</span>
+      <div className={this.renderClassName(message)}>
+        <div className="message-bubble">{message.MessageText}</div>
+        <div className="message-meta">
+          <span>{message.SentDate}</span>
+          <span>{message.Author}</span>
+        </div>
       </div>
     );
   }
